13c1e9eaae4ddffb56b129a6ae2aadb8686658a3
Louis Jeronimo <louis.jeronimo.etu@univ-lille.fr>
2021-01-19 10:21:21 +0100

    socket.c

---
+++ webserver/socket.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+
+int creer_serveur(int port){
+  int socket_serveur;
+
+  socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+  if(socket_serveur == -1){
+    perror("socket_serveur");
+    return -1;
+  }
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;/* Socket ipv4 */
+  saddr.sin_port = htons(port);/* Port d'écoute */
+  saddr.sin_addr.s_addr = INADDR_ANY;/* écoute sur toutes les interfaces */
+  if(bind(socket_serveur, (struct sockaddr*)&saddr,sizeof(saddr)) == -1){
+    perror("bind socket_serveur");
+    return -1;
+  }
+  if(listen(socket_serveur, 10) == -1){
+    perror("listen socket_serveur");/* traitement d'erreur */
+    return -1;
+  }
+  return socket_serveur;
+}

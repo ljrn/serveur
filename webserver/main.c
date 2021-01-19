b65f4b86830fa81f8d2dd02c975c370de18df230
Louis Jeronimo <louis.jeronimo.etu@univ-lille.fr>
2021-01-19 12:01:39 +0100

    zombies

--- webserver/main.c
+++ webserver/main.c
@@ -4,9 +4,21 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <signal.h>
+#include <wait.h>
 #include "socket.h"
 
+void traitement_signal(int sig){
+  int status;
+  waitpid(sig,&status,0);
+}
 void initialiser_signaux(void){
+  struct sigaction sa;
+  sa.sa_handler = traitement_signal;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if(sigaction(SIGCHLD, &sa, NULL) == -1){
+    perror("sigaction(SIGCHLD)");
+  }
   if(signal(SIGPIPE, SIG_IGN) == SIG_ERR){
     perror("signal");
   }

cde0ba81379321ab4c0655a10c89fb643f7bc030
Louis Jeronimo <louis.jeronimo.etu@univ-lille.fr>
2021-01-26 11:43:32 +0100

    6.1

--- webserver/main.c
+++ webserver/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -23,6 +24,7 @@ void initialiser_signaux(void){
     perror("signal");
   }
 }
+
 char *renvoie_reponse(int bad){
   if(bad==400){
     return "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n";
@@ -30,6 +32,30 @@ char *renvoie_reponse(int bad){
     return "HTTP/1.1 404 Not Found\r\n";
   }
   return "HTTP/1.1 200 OK\r\nContent-Length: 20\r\n";
+}
+char *fgets_or_exit(char *buffer, int size, FILE *stream){
+  int ligne=0;
+  int bad = 0;
+  while(fgets(buffer, size-1, stream)!=NULL){
+    if(strcmp(buffer,"\r\n")!=0){
+      char *err400="GET / HTTP/1.1\r\n";
+      char *err404="GET /inexistant HTTP/1.1\r\n";
+      if(ligne==0){
+	if(strcmp(buffer,err404)==0){
+	  bad=404;
+	}
+	else if(strcmp(buffer,err400)!=0){
+	  bad=400;
+	}
+	ligne++;
+      }
+    }else{
+      char *rep=renvoie_reponse(bad);
+      return rep;
+    }
+  }
+  exit(0);
+  return NULL;
 }
 
 int main(void){
@@ -39,32 +65,12 @@ int main(void){
     initialiser_signaux();
     socket_client = accept(socket_serveur, NULL, NULL);
     if(socket_client == -1){
-      perror("accept");/* traitement d'erreur */
-    }/* On peut maintenant dialoguer avec le client */
+      perror("accept");
+    }
     FILE *client=fdopen(socket_client, "a+");
     if(fork()!=0){
       char buffer[256];
-      int ligne=0;
-      int nonVide=0;
-      int bad = 0;
-      while(nonVide!=1 && fgets(buffer, 255, client)!=NULL){
-	if(strcmp(buffer,"\r\n")!=0){
-	  char *err400="GET / HTTP/1.1\r\n";
-	  char *err404="GET /inexistant HTTP/1.1\r\n";
-	  if(ligne==0){
-	    if(strcmp(buffer,err404)==0){
-	      bad=404;
-	    }
-	    else if(strcmp(buffer,err400)!=0){
-	      bad=400;
-	    }
-	    ligne++;
-	  }
-	}else{
-	  nonVide=1;
-	}
-      }
-      char *rep=renvoie_reponse(bad);
+      char *rep=fgets_or_exit(buffer,255,client);
       fprintf(client, "%s\n", rep);
       fflush(client);
     }else{
